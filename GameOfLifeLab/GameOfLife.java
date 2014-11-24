cf780a47f81330914d0c7647481594dfa1f9de17
rjnorton <robnorton@wowway.com>
2014-11-23 23:08:02 -0600

    Game of life lab

--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -4,27 +4,54 @@ import info.gridworld.actor.Rock;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.BoundedGrid;
 import info.gridworld.grid.Location;
+import java.util.ArrayList;
 
 /**
  * Game of Life starter code. Demonstrates how to create and populate the game using the GridWorld framework.
  * Also demonstrates how to provide accessor methods to make the class testable by unit tests.
  * 
- * @author @gcschmit
- * @version 18 July 2014
+ * @author @Rob Norton
+ * @version 10 November 2014
  */
 public class GameOfLife
 {
     // the world comprised of the grid that displays the graphics for the game
     private ActorWorld world;
+    private ActorWorld initialworld;
     
     // the game board will have 5 rows and 5 columns
-    private final int ROWS = 5;
-    private final int COLS = 5;
+    private final int ROWS = 22;
+    private final int COLS = 22;
     
     // constants for the location of the three cells initially alive
-    private final int X1 = 0, Y1 = 2;
-    private final int X2 = 2, Y2 = 0;
-    private final int X3 = 2, Y3 = 1;
+    private final int X1 = 11, Y1 = 5;
+    private final int X2 = 11, Y2 = 6;
+    private final int X3 = 11, Y3 = 7;
+    private final int X4 = 10, Y4 = 8;
+    private final int X5 = 12, Y5 = 8;
+    private final int X6 = 9, Y6 = 9;
+    private final int X7 = 13, Y7 = 9;
+    private final int X8 = 8, Y8 = 10;
+    private final int X9 = 14, Y9 = 10;
+    private final int X10 = 5, Y10 = 11;
+    private final int X11 = 6, Y11 = 11;
+    private final int X12 = 7, Y12 = 11;
+    private final int X14 = 10, Y14 = 11;
+    private final int X15 = 12, Y15 = 11;
+    private final int X16 = 15, Y16 = 11;
+    private final int X17 = 16, Y17 = 11;
+    private final int X18 = 17, Y18 = 11;
+    private final int X19 = 11, Y19 = 10;
+    private final int X20 = 11, Y20 = 12;
+    private final int X21 = 8, Y21 = 12;
+    private final int X22 = 14, Y22 = 12;
+    private final int X23 = 13, Y23 = 13;
+    private final int X24 = 9, Y24 = 13;
+    private final int X25 = 10, Y25 = 14;
+    private final int X26 = 12, Y26 = 14;
+    private final int X27 = 11, Y27 = 15;
+    private final int X28 = 11, Y28 = 16;
+    private final int X29 = 11, Y29 = 17;
 
     /**
      * Default constructor for objects of class GameOfLife
@@ -32,13 +59,14 @@ public class GameOfLife
      * @post    the game will be initialized and populated with the initial state of cells
      * 
      */
-    public GameOfLife()
+    public GameOfLife() throws InterruptedException
     {
         // create the grid, of the specified size, that contains Actors
         BoundedGrid<Actor> grid = new BoundedGrid<Actor>(ROWS, COLS);
         
         // create a world based on the grid
         world = new ActorWorld(grid);
+        initialworld = new ActorWorld(grid);
         
         // populate the game
         populateGame();
@@ -46,6 +74,12 @@ public class GameOfLife
         // display the newly constructed and populated world
         world.show();
         
+        for (int i = 0; i <=30; i++)
+        {
+            createNextGeneration();
+            world.show();
+            Thread.sleep(100);
+        }
     }
     
     /**
@@ -73,6 +107,109 @@ public class GameOfLife
         Rock rock3 = new Rock();
         Location loc3 = new Location(X3, Y3);
         grid.put(loc3, rock3);
+        
+        Rock rock4 = new Rock();
+        Location loc4 = new Location(X4, Y4);
+        grid.put(loc4, rock4);
+                
+        Rock rock5 = new Rock();
+        Location loc5 = new Location(X5, Y5);
+        grid.put(loc5, rock5);
+        
+        Rock rock6 = new Rock();
+        Location loc6 = new Location(X6, Y6);
+        grid.put(loc6, rock6);
+        
+        Rock rock7 = new Rock();
+        Location loc7 = new Location(X7, Y7);
+        grid.put(loc7, rock7);
+        
+        Rock rock8 = new Rock();
+        Location loc8 = new Location(X8, Y8);
+        grid.put(loc8, rock8);
+        
+        Rock rock9 = new Rock();
+        Location loc9 = new Location(X9, Y9);
+        grid.put(loc9, rock9);
+        
+        Rock rock10 = new Rock();
+        Location loc10 = new Location(X10, Y10);
+        grid.put(loc10, rock10);
+        
+        Rock rock11 = new Rock();
+        Location loc11 = new Location(X11, Y11);
+        grid.put(loc11, rock11);
+        
+        Rock rock12 = new Rock();
+        Location loc12 = new Location(X12, Y12);
+        grid.put(loc12, rock12);
+        
+
+        
+        Rock rock14 = new Rock();
+        Location loc14 = new Location(X14, Y14);
+        grid.put(loc14, rock14);
+        
+        Rock rock15 = new Rock();
+        Location loc15 = new Location(X15, Y15);
+        grid.put(loc15, rock15);
+        
+        Rock rock16 = new Rock();
+        Location loc16 = new Location(X16, Y16);
+        grid.put(loc16, rock16);
+        
+        Rock rock17 = new Rock();
+        Location loc17 = new Location(X17, Y17);
+        grid.put(loc17, rock17);
+        
+        Rock rock18 = new Rock();
+        Location loc18 = new Location(X18, Y18);
+        grid.put(loc18, rock18);
+        
+        Rock rock19 = new Rock();
+        Location loc19 = new Location(X19, Y19);
+        grid.put(loc19, rock19);
+        
+        Rock rock20 = new Rock();
+        Location loc20 = new Location(X20, Y20);
+        grid.put(loc20, rock20);
+        
+        Rock rock21 = new Rock();
+        Location loc21 = new Location(X21, Y21);
+        grid.put(loc21, rock21);
+        
+        Rock rock22 = new Rock();
+        Location loc22 = new Location(X22, Y22);
+        grid.put(loc22, rock22);
+        
+        Rock rock23 = new Rock();
+        Location loc23 = new Location(X23, Y23);
+        grid.put(loc23, rock23);
+        
+        Rock rock24 = new Rock();
+        Location loc24 = new Location(X24, Y24);
+        grid.put(loc24, rock24);
+        
+        Rock rock25 = new Rock();
+        Location loc25 = new Location(X25, Y25);
+        grid.put(loc25, rock25);
+        
+        Rock rock26 = new Rock();
+        Location loc26 = new Location(X26, Y26);
+        grid.put(loc26, rock26);
+        
+        Rock rock27 = new Rock();
+        Location loc27 = new Location(X27, Y27);
+        grid.put(loc27, rock27);
+        
+        Rock rock28 = new Rock();
+        Location loc28 = new Location(X28, Y28);
+        grid.put(loc28, rock28);
+        
+        Rock rock29 = new Rock();
+        Location loc29 = new Location(X29, Y29);
+        grid.put(loc29, rock29);
+        
     }
 
     /**
@@ -83,7 +220,7 @@ public class GameOfLife
      * @post    the world has been populated with a new grid containing the next generation
      * 
      */
-    private void createNextGeneration()
+    public void createNextGeneration()
     {
         /** You will need to read the documentation for the World, Grid, and Location classes
          *      in order to implement the Game of Life algorithm and leverage the GridWorld framework.
@@ -91,10 +228,40 @@ public class GameOfLife
         
         // create the grid, of the specified size, that contains Actors
         Grid<Actor> grid = world.getGrid();
+        BoundedGrid<Actor> gridnew = new BoundedGrid<Actor>(ROWS, COLS);
         
-        // insert magic here...
         
-    }
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                Location newloc = new Location(i,j);
+                Actor newact = grid.get(newloc);
+                if (newact == null)
+                {
+                    ArrayList<Actor> neighbors = grid.getNeighbors(newloc);
+                    int len = neighbors.size();
+                    if (len == 3)
+                   {
+                    Rock newrock = new Rock();
+                    gridnew.put(newloc, newrock);
+                   }
+                }
+                
+                else
+                {
+                    ArrayList<Actor> neighbors1 = grid.getNeighbors(newloc);
+                    int length = neighbors1.size();
+                    if (length == 2 || length == 3)
+                    {
+                        Rock newrock1 = new Rock();
+                        gridnew.put(newloc,newrock1);
+                    }
+                }
+                }
+            }
+        world.setGrid(gridnew);
+        }
     
     /**
      * Returns the actor at the specified row and column. Intended to be used for unit testing.
@@ -131,12 +298,26 @@ public class GameOfLife
         return COLS;
     }
     
+    /**
+     * Returns the initial actor at the specified row and column. Intended to be used for unit testing.
+     *
+     * @param   row the row (zero-based index) of the actor to return
+     * @param   col the column (zero-based index) of the actor to return
+     * @pre     the grid has been created
+     * @return  the actor at the specified row and column
+     */
+    public Actor getInitialActor(int row, int col)
+    {
+        Location loc = new Location(row, col);
+        Actor actor = initialworld.getGrid().get(loc);
+        return actor;
+    }
     
     /**
      * Creates an instance of this class. Provides convenient execution.
      *
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         GameOfLife game = new GameOfLife();
     }
--- GameOfLifeLab/GameOfLifeTest.java
+++ GameOfLifeLab/GameOfLifeTest.java
@@ -41,17 +41,25 @@ public class GameOfLifeTest
     }
 
     @Test
-    public void testInitialState()
+    public void testInitialState() throws InterruptedException
     {
         /* expected pattern for initial state
          *  (X: alive; -: dead)
          * 
-         *    0 1 2 3 4
-         *  0 - - X - -
-         *  1 - - - - -
-         *  2 X X - - -
-         *  3 - - - - -
-         *  4 - - - - -
+         *    0 1 2 3 4 5 6 7 8 9 10 11 12
+         *  0 - - - - - - x - - - - - -
+         *  1 - - - - - - x - - - - - -
+         *  2 - - - - - - x - - - - - -
+         *  3 - - - - - x - x - - - - -
+         *  4 - - - - x - - - x - - - -
+         *  5 - - - x - - x - - x - - -
+         *  6 x x x - - x - x - - x x x
+         *  7 - - - x - - x - - x - - -
+         *  8 - - - - x - - - x - - - -
+         *  9 - - - - - x - x - - - - -
+         *  10- - - - - - x - - - - - - 
+         *  11- - - - - - x - - - - - -
+         *  12- - - - - - x - - - - - -
          *  
          */
         
@@ -64,12 +72,38 @@ public class GameOfLifeTest
             for(int col = 0; col < COLS; col++)
             {
                 // in this example, an alive cell has a non-null actor and a dead cell has a null actor
-                Actor cell = game.getActor(row, col);
+                Actor cell = game.getInitialActor(row, col);
 
                 // if the cell at the current row and col should be alive, assert that the actor is not null
-                if(     (row == 0 && col == 2) ||
-                        (row == 2 && col == 0) ||
-                        (row == 2 && col == 1))
+                if(     (row == 5 && col == 11) ||
+                        (row == 6 && col == 11) ||
+                        (row == 7 && col == 11) ||
+                        (row == 8 && col == 10) ||
+                        (row == 8 && col == 12) ||
+                        (row == 9 && col == 9) ||
+                        (row == 9 && col == 13) ||
+                        (row == 10 && col == 8) ||
+                        (row == 10 && col == 14) ||
+                        (row == 11 && col == 5) ||
+                        (row == 11 && col == 6) ||
+                        (row == 11 && col == 7) ||
+                        (row == 11 && col == 15) ||
+                        (row == 11 && col == 16) ||
+                        (row == 11 && col == 17) ||
+                        (row == 11 && col == 10) ||
+                        (row == 11 && col == 12) ||
+                        (row == 10 && col == 11) ||
+                        (row == 12 && col == 11) ||
+                        (row == 12 && col == 8) ||
+                        (row == 12 && col == 14) ||
+                        (row == 13 && col == 9) ||
+                        (row == 13 && col == 13) ||
+                        (row == 14 && col == 10) ||
+                        (row == 14 && col == 12) ||
+                        (row == 15 && col == 11) ||
+                        (row == 16 && col == 11) ||
+                        (row == 17 && col == 11))
+
                 {
                     assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
                 }
@@ -82,12 +116,69 @@ public class GameOfLifeTest
     }
 
     @Test
-    public void testFinalState()
-    {
-        /* verify that the actual pattern matches the expected pattern after 3 generations         *  
+    public void testFinalState() throws InterruptedException
+    /* expected pattern for final state
+         *  (X: alive; -: dead)
+         * 
+         *    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+         *  0 - - - - - - - - - - - - - - - - - - 
+         *  1 - - - - - - - - - - - - - - - - - - 
+         *  2 - - - - - - - - - - - - - - - - - - 
+         *  3 - - - - - - - - - - - - - - - - - - 
+         *  4 - - - - - - - - - X X X - - - - - - 
+         *  5 - - - - - - - - - - - - - - - - - - 
+         *  6 - - - - - - - - - - - - - - - - - - 
+         *  7 - - - - - - - - - - - - - - - - - - 
+         *  8 - - - - - - - - - - - - - - - - - - 
+         *  9 - - - - - - - - - - - - - - - - - - 
+         *  10- - - X - - - - - - - - - - - - - X 
+         *  11- - - X - - - - - - - - - - - - - X 
+         *  12- - - X - - - - - - - - - - - - - X 
+         *  13- - - - - - - - - - - - - - - - - - 
+         *  14- - - - - - - - - - - - - - - - - - 
+         *  15- - - - - - - - - - - - - - - - - - 
+         *  16- - - - - - - - - - - - - - - - - - 
+         *  17- - - - - - - - - - - - - - - - - - 
+         *  18- - - - - - - - - X X X - - - - - - 
          */
+    {
+        GameOfLife game1 = new GameOfLife();
+        final int ROWS = game1.getNumRows();
+        final int COLS = game1.getNumCols();
         
-        // ...
-    }
-}
+        for(int row = 0; row < ROWS; row++)
+        {
+            for(int col = 0; col < COLS; col++)
+            {
+                // in this example, an alive cell has a non-null actor and a dead cell has a null actor
+                Actor cell = game1.getActor(row, col);
 
+                // if the cell at the current row and col should be alive, assert that the actor is not null
+                if(     (row == 10 && col == 4) ||
+                        (row == 11 && col == 4) ||
+                        (row == 12 && col == 4) ||
+                        (row == 4 && col == 10) ||
+                        (row == 4 && col == 11) ||
+                        (row == 4 && col == 12) ||
+                        (row == 10 && col == 18) ||
+                        (row == 11 && col == 18) ||
+                        (row == 12 && col == 18) ||
+                        (row == 4 && col == 10) ||
+                        (row == 4 && col == 11) ||
+                        (row == 4 && col == 12) ||
+                        (row == 18 && col == 10) ||
+                        (row == 18 && col == 11)||
+                        (row == 18 && col == 12))
+                        
+                {
+                    assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
+                }
+                
+                else
+                {
+                    assertNull("expected dead cell at (" + row + ", " + col + ")", cell);
+                }
+            }
+        }
+    }
+    }
